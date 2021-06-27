7cbe9d454cbfe620826cdfb1e091b8ef873d3999
jbcoolness <julio.bertty@gmail.com>
2021-06-27 18:53:05 -0500

    added login endpoint

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const seqielize = require('./conexion');
 //Routes
 const usersRoutes = require('./routers/users.routes')
 
-//Middleware
+// Middleware
 app.use(express.json());
+// valida si se está logiando con correo electrónico
+// const isEmail = (req, res, next) => {
+//     expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//     if ( !expr.test(req.body.email) ) { req.isEmail = false } else { req.isEmail = true };
+//     next()
+// }
 
-//Routes use
+// Routes use
 app.use('/api/v1/users', usersRoutes);
 
 
-//Server
+// Server
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 });
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -6,7 +6,7 @@ const registerUser = async (req, res) => {
     console.log(dataInsertUser)
 
     try {
-        const result = await sequelize.query("INSERT INTO users(`user`, full_name, email, phone, address, password) VALUES(?, ?, ?, ?, ?, ?);",
+        const result = await sequelize.query("INSERT INTO users(`user`, full_name, email, phone, address, password) VALUES (?, ?, ?, ?, ?, ?);",
         { replacement: dataInsertUser, type: sequelize.QueryTypes.INSERT })
         console.log(result);
         res.status(201).json({
@@ -23,27 +23,51 @@ const registerUser = async (req, res) => {
     }
 };
 
-const loginUser = async (req, res) => {
-
-};
-
-const getUser = async (req, res) => {
-    try {
-        const result = await sequelize.query('SELECT * FROM users', 
-        {type:sequelize.QueryTypes.SELECT})
-        console.log(result)
-        res.status(200).json({
-            'msg': true,
-            'data':result
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            'msg': false,
-            data: error
-        })
-    }
+const validarEmail = (email) => {
+    expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if ( !expr.test(email) ) { return false } else { return true };
 }
+
+const loginUser = async (req, res) => {
+    const { user_email, password} = req.body;
+
+    // console.log(req.isEmail)
+    try {
+        console.log(user_email)
+        const result = await sequelize.query(`select * from users u where (email = '${user_email}' or user = '${user_email}') and password = '${password}';`,
+        { type: sequelize.QueryTypes.SELECT });
+        console.log(result)
+        res.status(200).json( {
+            'msg': true,
+            'data': result
+        })
+        } catch (error) {
+            console.log(error)
+            res.status(400).json( {
+                'msg': false,
+                'data': 'usuario no encontrado, valide la información'
+            })
+        }
+        
+    };
+
+    const getUser = async (req, res) => {
+        try {
+            const result = await sequelize.query('SELECT * FROM users', 
+            {type:sequelize.QueryTypes.SELECT})
+            console.log(result)
+            res.status(200).json({
+                'msg': true,
+                'data':result
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({
+                'msg': false,
+                data: error
+            })
+        }
+    }
 
 const updateUser = async (req, res) => {
 
